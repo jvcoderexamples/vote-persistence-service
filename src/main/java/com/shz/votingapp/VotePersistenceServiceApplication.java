7519e7f8246c71fdcac5e2200bbeea337fe582fc
unknown <shahbazakbar@gmail.com>
2023-01-02 00:10:45 +0500

    Initial Commit

---
+++ src/main/java/com/shz/votingapp/VotePersistenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shz.votingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotePersistenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotePersistenceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shz/votingapp/VotePersistenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shz.votingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotePersistenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
